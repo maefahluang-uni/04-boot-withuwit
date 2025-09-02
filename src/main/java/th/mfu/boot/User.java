0d7da2742575ed654ae5e1a30c875a781814af66
withuwit <6731503037@lamduan.mfu.ac.th>
2025-09-02 14:41:29 +0000

    done

--- src/main/java/th/mfu/boot/User.java
+++ src/main/java/th/mfu/boot/User.java
@@ -1,6 +1,36 @@
 package th.mfu.boot;
 
 public class User {
+    private String username;
+    private String displayname;
+    private String email;
 
-    
+    public User() {}
+
+    public User(String username, String displayname, String email) {
+        this.username = username;
+        this.displayname = displayname;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getDisplayname() {
+        return displayname;
+    }
+    public void setDisplayname(String displayname) {
+        this.displayname = displayname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
--- src/main/java/th/mfu/boot/UserController.java
+++ src/main/java/th/mfu/boot/UserController.java
@@ -6,41 +6,36 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/users")
 public class UserController {
 
-    public static Map<String, User> users = new HashMap<String, User>();
+    public static Map<String, User> users = new HashMap<>();
 
-    @PostMapping
-    public ResponseEntity<String> registerUser( User user) {
-      //TODO
-    if (users.containsKey(user.getUsername())) {
+    // POST /users
+    @PostMapping("/users")
+    public ResponseEntity<String> registerUser(@RequestBody User user) {
+        if (users.containsKey(user.getUsername())) {
             return new ResponseEntity<>("Username already exists", HttpStatus.CONFLICT);
         }
         users.put(user.getUsername(), user);
         return new ResponseEntity<>("User registered successfully", HttpStatus.OK);
     }
-    @GetMapping
+
+    // GET /users
+    @GetMapping("/users")
     public ResponseEntity<Collection<User>> list() {
-        //TODO
         return new ResponseEntity<>(users.values(), HttpStatus.OK);
     }
-    @GetMapping("/{username}")
-    public ResponseEntity<User> getUser(String username) {
-        //TODO
+
+    // GET /users/{username}
+    @GetMapping("/users/{username}")
+    public ResponseEntity<User> getUser(@PathVariable String username) {
         User user = users.get(username);
         if (user == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
-    
 }
